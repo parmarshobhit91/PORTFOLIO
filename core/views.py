a61888dd04f9ae7c0ba3751bf25ed8c1448dda30
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2026-01-25 23:16:38 +0530

    admin created again

--- core/views.py
+++ core/views.py
@@ -6,16 +6,6 @@ from .utils import *
 def home_view(request):
     return render(request, 'core/home.html')
 
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def admin_creation(request):
-    User.objects.create_superuser(
-        username="admin",
-        email="sp@example.com",
-        password="admin123"
-    )
-    return HttpResponse("Admin created")
 
 
 def about_view(request):
