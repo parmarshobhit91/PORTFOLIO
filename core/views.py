7e74f75e6046abd5ed00d991b710101c0881d5a0
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2026-01-27 19:11:25 +0530

    debugging 500 error on contact form

--- core/views.py
+++ core/views.py
@@ -15,22 +15,23 @@ def about_view(request):
 
 def contact_view(request):
     if request.method == "POST":
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        contact_number = request.POST.get('contact_number')
+        print("CONTACT FORM SUBMITTED")
+        # name = request.POST.get('name')
+        # email = request.POST.get('email')
+        # message = request.POST.get('message')
+        # contact_number = request.POST.get('contact_number')
 
-        try:
-            send_email_to_me(name, email, contact_number, message)
-            # send_email_to_contacted_person(email)
-        except Exception as e:
-            # Log error but DO NOT crash
-            print("Email sending failed:", e)
+        # try:
+        #     send_email_to_me(name, email, contact_number, message)
+        #     # send_email_to_contacted_person(email)
+        # except Exception as e:
+        #     # Log error but DO NOT crash
+        #     print("Email sending failed:", e)
 
-        messages.success(
-            request,
-            "Thank you for contacting me. I will get back to you shortly."
-        )
-        return redirect("contact")  # IMPORTANT
+        # messages.success(
+        #     request,
+        #     "Thank you for contacting me. I will get back to you shortly."
+        # )
+        # return redirect("contact")  # IMPORTANT
 
     return render(request, 'core/contact.html')
